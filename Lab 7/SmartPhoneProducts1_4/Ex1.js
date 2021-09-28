c458781e52d8956fa11b0f2d4632667d084b8848
MatteHue808 <calulotm@hawaii.edu>
2021-09-27 20:51:23 -1000

    Labs

---
+++ Lab 7/SmartPhoneProducts1_4/Ex1.js
@@ -0,0 +1,8 @@
+var age =  20;
+var fav_num = 8;
+var day_of_birth = 13
+var month_of_birth = 4;
+
+console.log( age + fav_num / day_of_birth * month_of_birth);
+
+// console.log( (age + fav_num) / day_of_birth * month_of_birth);
