664cbaabdbaa845626ba4a2c00055d9b94b89b9c
MatteHue808 <calulotm@hawaii.edu>
2021-10-12 09:28:17 -1000

    Update Ex2.js

--- Lab 8/Ex2.js
+++ Lab 8/Ex2.js
@@ -5,7 +5,7 @@ while(count++ < num_products) {
     if(count> num_products/2) {
         console.log("Don't ask for anything else!");
         process.exit();
-        break;
+        // break;
     }
     if(count > 0.25*num_products && count < 0.75*num_products) {
         console.log(`Item #${count} is sold out!`);
