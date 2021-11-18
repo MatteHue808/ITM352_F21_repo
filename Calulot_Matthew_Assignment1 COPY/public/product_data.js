6441da06d2b92478e63e11d45b0c3a614f0e8247
MatteHue808 <calulotm@hawaii.edu>
2021-11-18 09:21:20 -1000

    Assignment1FinalRevision

---
+++ Calulot_Matthew_Assignment1 COPY/public/product_data.js
@@ -0,0 +1,42 @@
+products_array = [
+    {
+        "product": "Lamy Safari Fountain Pen",
+        "brand": "LAMY",
+        "price": 29.99,
+        "quantity_available": 100,
+        "img": "https://i5.walmartimages.com/asr/92b6d920-bbc3-47dc-a6d5-f1d612d480f3.f710eaa494b48fe20b4d95a5e4043c73.jpeg"
+    },
+    {
+        "product": "TWSBI ECO Clear Fountain Pen",
+        "brand": "TWSBI",
+        "price": 32.99,
+        "quantity_available": 120,
+        "img": "https://cdn.shopify.com/s/files/1/0535/9633/products/ecoclear_1024x1024.png?v=1475166920"
+    },
+    {
+        "product": "Kaweco Brass Sport Fountain Pen",
+        "brand": "Kaweco",
+        "price": 72.99,
+        "quantity_available": 100,
+        "img": "https://d15bv9e9f3al6i.cloudfront.net/imgs/products/cp/950_constW/KW43592-ZZZ~Kaweco-AL-Sport-Fountain-Pen-Brass_P1.jpg"
+    },
+    {
+        "product": "Platinum #3766 Century Fountain Pen",
+        "brand": "Platinum",
+        "price": 175.00,
+        "quantity_available": 80,
+        "img": "https://nibsmith.com/wp-content/uploads/2018/09/platinum-3776-Laurel-green-fountain-pen-nibsmith.jpg"
+    },
+    {
+        "product": "Sailor 1911 Standard Fountain Pen",
+        "brand": "Sailor",
+        "price": 225.00,
+        "quantity_available": 90,
+        "img": "https://d15bv9e9f3al6i.cloudfront.net/imgs/products/cp/950_constW/SR60957-ZZZ~Sailor-1911-Standard-Fountain-Pen-Black-with-Gold-Trim-21K-Nib_P1.jpg"
+    }
+]
+
+//if inputs are not undefined export products array data
+if (typeof module != 'undefined') {
+    module.exports.products_array = products_array;
+}
